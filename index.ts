4ab2978b1296a331f35d96f1ec8a3ac8c0294bab
winalife-kp <137119640+winalife-kp@users.noreply.github.com>
2023-06-20 00:27:18 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,34 @@
-// Import stylesheets
-import './style.css';
+interface SomeInterface {
+  id: string;
+  value: unknown;
+}
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+const variable1 = {
+  id: '1',
+  value: 2, // value: 'some_string', -> 1. what will happen?
+}
+
+//variable1.value = 'some_string'    -> 2. what will happen?
+
+
+const variable2: SomeInterface = {
+  id: '1',
+  value: 2, // value: 'some_string', -> 3. what will happen?
+};
+
+//variable2.value = 'some_string'    -> 4. what will happen?
+
+const variable3 = {
+  id: '1',
+  value: 2, // value: 'some_string', -> 5. what will happen?
+} as SomeInterface;
+
+//variable3.value = 'some_string'    -> 6. what will happen?
+
+
+const variable4 = {
+  id: '1',
+  value: 2, // value: 'some_string', -> 7. what will happen?
+} satisfies SomeInterface;
+
+//variable4.value = 'some_string'    -> 8. what will happen?
