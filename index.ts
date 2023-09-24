c4aa2fa963e9de03dd2fd5cc8511f572fbf7c62e
winalife-kp <137119640+winalife-kp@users.noreply.github.com>
2023-09-25 01:25:45 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -3,21 +3,19 @@ interface SomeInterface {
   value: unknown;
 }
 
-
 const variable1 = {
   id: '1',
   value: 2,
-}
+};
 //variable1.value = 'some_string'
 /*
   1. What is the type of variable1?
   2. What will happen after we uncomment line 11? 
 */
 
-
 const variable2: SomeInterface = {
   id: '1',
-  value: 2, 
+  value: 2,
 };
 // variable2.value = 'some_string'
 /*
@@ -25,15 +23,13 @@ const variable2: SomeInterface = {
   4. What will happen after we uncomment line 22? 
 */
 
-
 const variable3: SomeInterface = {
   id: '1',
-  // value: 'some_string', 
+  // value: 'some_string',
 };
 /*
   5. What will happen after we uncomment line 31?
 */
-
 
 const variable4 = {
   id: '1',
@@ -45,7 +41,6 @@ const variable4 = {
   7. What will happen after we uncomment line 42? 
 */
 
-
 const variable5 = {
   id: '1',
   // value: 'some_string',
@@ -54,11 +49,10 @@ const variable5 = {
   8. What will happen after we uncomment line 51? 
 */
 
-
-const variable6 = {
-  id: '1',
-  value: 2, 
-} satisfies SomeInterface;
+// const variable6 = {
+//   id: '1',
+//   value: 2,
+// } satisfies SomeInterface;
 // variable6.value = 'some_string'
 /*
   9. What is the type of variable6?
